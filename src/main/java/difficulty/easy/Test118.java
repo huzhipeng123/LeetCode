2686479b1ebe169f8f8bd69cc8fae300cf0ffce9
huzhpm <huzhpm@yonyou.com>
2019-10-12 23:39:16 +0800

    Test118未完成

---
+++ src/main/java/difficulty/easy/Test118.java
@@ -0,0 +1,26 @@
+package difficulty.easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author huzhpm
+ * @Date 2019/10/12 23:35
+ * @Version 1.0
+ * @Content 杨辉三角
+ */
+public class Test118 {
+
+    /**
+     * 给定一个非负整数 numRows，生成杨辉三角的前 numRows 行。
+     * @param numRows
+     * @return
+     */
+    public List<List<Integer>> generate(int numRows){
+        if(numRows < 1){
+            return null;
+        }
+        List<List<Integer>> resultList = new ArrayList<List<Integer>>();
+        for(int numRows)
+    }
+}
